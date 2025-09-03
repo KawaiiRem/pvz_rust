44a8eccc0e37b2b33f5f1ebab8d3cde331b91781
HoangVu <hoang.vu@successsoftware.global>
2025-09-03 13:57:04 +0700

    implement slowpeashooter and slow effect for zombies

--- src/projectile.rs
+++ src/projectile.rs
@@ -1,6 +1,12 @@
 use macroquad::prelude::*;
-use crate::constants::*;
+use crate::{constants::*, projectile};
 use crate::zombie::{Zombie, ZombieState};
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+pub enum ProjectileType {
+    Normal,
+    Slow,
+}
 
 #[derive(Debug)]
 pub struct Projectile {
@@ -9,16 +15,28 @@ pub struct Projectile {
     pub speed: f32,
     pub damage: i32,
     pub active: bool,
+    pub projectile_type: ProjectileType,
 }
 
 impl Projectile {
-    pub fn new(x: f32, y: f32) -> Self {
-        Self {
-            x,
-            y,
-            speed: 200.0, // pixels per second
-            damage: 20,
-            active: true,
+    pub fn new(x: f32, y: f32, projectile_type: ProjectileType) -> Self {
+        match projectile_type{
+            ProjectileType::Normal => Self {
+                x,
+                y,
+                speed: 300.0, // pixels per second
+                damage: 20,
+                active: true,
+                projectile_type,
+            },
+            ProjectileType::Slow => Self {
+                x,
+                y,
+                speed: 300.0, // pixels per second
+                damage: 10,
+                active: true,
+                projectile_type,
+            },
         }
     }
 
@@ -44,6 +62,11 @@ impl Projectile {
             if (zombie.y - self.y).abs() < TILE_HEIGHT / 2.0 &&
                (zombie.x - self.x).abs() < 20.0 { // hitbox
                 zombie.health -= self.damage;
+
+                if self.projectile_type == ProjectileType::Slow {
+                    zombie.apply_slow(2.5);
+                }
+
                 self.active = false;
                 break;
             }
@@ -51,8 +74,12 @@ impl Projectile {
     }
 
     pub fn draw(&self) {
-        if self.active {
-            draw_circle(self.x, self.y, 5.0, DARKGREEN);
+        if !self.active {
+            return;
+        }
+        match self.projectile_type {
+            ProjectileType::Normal => draw_circle(self.x, self.y, 5.0, DARKGREEN),
+            ProjectileType::Slow   => draw_circle(self.x, self.y, 6.0, DARKBLUE),
         }
     }
 }
